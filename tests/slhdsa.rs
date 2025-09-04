8885e62c34bfc6214f50962038061c33e16dd704
Nicola Tuveri <nic.tuv@gmail.com>
2025-09-04 20:52:07 +0300

    feat(tests): add keygen/sign/verify tests for the bindings using rand
    - Add comprehensive tests for SLH DSA key generation, signing, and
      verification.
    - Integrate the `rand` crate for random number generation in test
      helpers.
    
    
    Signed-off-by: Nicola Tuveri <nic.tuv@gmail.com>
    Change-Id: I6a6a6964ad322b7a746b1b5fe43e42c021c4a041

--- tests/slhdsa.rs
+++ tests/slhdsa.rs
@@ -1,5 +1,3 @@
-use slhdsa_c_rs::bindings;
-
 pub(crate) mod common {
     pub(crate) type OurError = anyhow::Error;
     pub(crate) type OurResult<T> = anyhow::Result<T>;
@@ -35,12 +33,35 @@ pub(crate) mod common {
     }
 }
 
+mod helpers {
+    use rand::{Rng, SeedableRng};
+
+    pub unsafe extern "C" fn randombytes(x: *mut u8, xlen: usize) -> ::core::ffi::c_int {
+        const SUCCESS: ::core::ffi::c_int = 0;
+
+        let x = {
+            let nonnull_pointer = core::ptr::NonNull::new(x).unwrap();
+            let mut x = core::ptr::NonNull::slice_from_raw_parts(nonnull_pointer, xlen);
+            let x = unsafe { x.as_mut() };
+            x
+        };
+
+        let mut rng = rand::rngs::StdRng::from_os_rng();
+
+        rng.fill(x);
+
+        SUCCESS
+    }
+}
+
 #[test]
 fn basic_bindings_test() {
+    use slhdsa_c_rs::bindings;
+
     common::setup().expect("Failed during initial setup");
 
     unsafe {
-        let prm = &bindings::slh_dsa_shake_128f;
+        let prm = &bindings::slh_dsa_shake_256s;
 
         let id = bindings::slh_alg_id(std::ptr::from_ref(prm));
         let id = core::ffi::CStr::from_ptr(id)
@@ -48,10 +69,95 @@ fn basic_bindings_test() {
             .expect("Invalid CStr");
         log::info!("id={:?}", id);
 
-        let pk_sz = bindings::slh_pk_sz(std::ptr::from_ref(prm));
-        log::info!("pk_sz={:?}", pk_sz);
-
         let sk_sz = bindings::slh_sk_sz(std::ptr::from_ref(prm));
         log::info!("sk_sz={:?}", sk_sz);
+        let pk_sz = bindings::slh_pk_sz(std::ptr::from_ref(prm));
+        log::info!("pk_sz={:?}", pk_sz);
+        let sig_sz = bindings::slh_sig_sz(std::ptr::from_ref(prm));
+        log::info!("sig_sz={:?}", sig_sz);
+
+        let mut sk: Vec<u8> = vec![0; sk_sz];
+        let mut pk: Vec<u8> = vec![0; pk_sz];
+
+        let rbg = helpers::randombytes;
+
+        let ret = bindings::slh_keygen(sk.as_mut_ptr(), pk.as_mut_ptr(), Some(rbg), prm);
+        assert_eq!(ret, 0);
+
+        log::info!("sk={:x?}", sk);
+        log::info!("pk={:x?}", pk);
+
+        let msg: &[u8] = &[1, 2, 3, 4, 5];
+        log::info!("msg={:?}", msg);
+
+        let ctx: &[u8] = &[];
+        log::info!("ctx={:x?}", ctx);
+
+        let mut sig: Vec<u8> = vec![0; sig_sz];
+        let addrnd = ::core::ptr::null();
+
+        let ret = bindings::slh_sign(
+            sig.as_mut_ptr(),
+            msg.as_ptr(),
+            msg.len(),
+            ctx.as_ptr(),
+            ctx.len(),
+            sk.as_ptr(),
+            addrnd,
+            prm,
+        );
+        assert_eq!(ret, sig_sz);
+        log::debug!("sig={:x?}", sig);
+
+        let ret = bindings::slh_verify(
+            msg.as_ptr(),
+            msg.len(),
+            sig.as_ptr(),
+            sig.len(),
+            ctx.as_ptr(),
+            ctx.len(),
+            pk.as_ptr(),
+            prm,
+        );
+        log::info!("slh_verify() returned {:x?} on happy path", ret);
+        assert_eq!(ret, 1);
+
+        {
+            let msg: &[u8] = &[1, 2, 3, 4, 5, 6];
+
+            let ret = bindings::slh_verify(
+                msg.as_ptr(),
+                msg.len(),
+                sig.as_ptr(),
+                sig.len(),
+                ctx.as_ptr(),
+                ctx.len(),
+                pk.as_ptr(),
+                prm,
+            );
+            log::info!("slh_verify() returned {:x?} on tampered message", ret);
+            assert_eq!(ret, 0);
+        }
+
+        {
+            let mut sk: Vec<u8> = vec![0; sk_sz];
+            let mut pk: Vec<u8> = vec![0; pk_sz];
+
+            let ret = bindings::slh_keygen(sk.as_mut_ptr(), pk.as_mut_ptr(), Some(rbg), prm);
+            assert_eq!(ret, 0);
+
+            let ret = bindings::slh_verify(
+                msg.as_ptr(),
+                msg.len(),
+                sig.as_ptr(),
+                sig.len(),
+                ctx.as_ptr(),
+                ctx.len(),
+                pk.as_ptr(),
+                prm,
+            );
+            log::info!("slh_verify() returned {:x?} on wrong pk", ret);
+            assert_eq!(ret, 0);
+        }
     }
 }
