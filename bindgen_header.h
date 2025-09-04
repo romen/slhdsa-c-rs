36de46e58e988800ea3a4398f27f35ef3a7ad715
Nicola Tuveri <nic.tuv@gmail.com>
2025-09-04 15:45:34 +0300

    feat: initial Rust FFI bindings for slhdsa-c
    Add Rust crate setup with build.rs to compile C sources and generate
    bindings using bindgen. Includes .gitignore updates, Cargo.toml, and
    Cargo.lock. C headers are included via bindgen_header.h. The generated
    bindings are exposed in src/lib.rs. This enables Rust integration with
    the slhdsa-c library.
    
    
    Signed-off-by: Nicola Tuveri <nic.tuv@gmail.com>
    Change-Id: I6a6a696487c71587983a431f9279f07cb2f253e6

---
+++ bindgen_header.h
@@ -0,0 +1,3 @@
+#include "slh_dsa.h"
+#include "slh_prehash.h"
+#include "slh_var.h"
---
+++ build.rs
@@ -0,0 +1,79 @@
+use glob::glob;
+
+const SLHDSA_C_PATH: &str = &"./slhdsa-c/";
+
+fn compile_c_sources() {
+    let include_path = std::path::PathBuf::from(SLHDSA_C_PATH);
+
+    let mut build = cc::Build::new();
+    build.include(&include_path);
+    build
+        .flag("-Wall")
+        .flag("-Wextra")
+        .flag("-Werror=unused-result")
+        .flag("-Wpedantic")
+        .flag("-Werror")
+        .flag("-Wmissing-prototypes")
+        .flag("-Wshadow")
+        .flag("-Wpointer-arith")
+        .flag("-Wredundant-decls")
+        .flag("-Wno-long-long")
+        .flag("-Wno-unknown-pragmas")
+        .flag("-O3")
+        .flag("-fomit-frame-pointer")
+        .flag("-std=c99")
+        .flag("-pedantic");
+
+    let pattern = include_path.clone().join("*.c");
+    let pattern = pattern.to_str().expect("Path not valid UTF-8");
+
+    for entry in glob(pattern).expect("Failed to read glob pattern") {
+        match entry {
+            Ok(path) => {
+                println!("cargo:rerun-if-changed={}", path.display());
+                build.file(path);
+            }
+            Err(e) => eprintln!("Glob error: {:?}", e),
+        }
+    }
+
+    build.compile("slhdsa-c"); // creates libslhdsa-c.a
+}
+
+fn generate_bindings() {
+    let include_path = std::path::PathBuf::from(SLHDSA_C_PATH);
+
+    let mut builder = bindgen::Builder::default();
+    builder = builder.clang_arg(format!("-I{}", include_path.display()));
+
+    let pattern = include_path.clone().join("*.h");
+    let pattern = pattern.to_str().expect("Path not valid UTF-8");
+
+    for entry in glob(pattern).expect("Failed to read glob pattern") {
+        match entry {
+            Ok(path) => {
+                println!("cargo:rerun-if-changed={}", path.display());
+                let path = path.to_str().expect("Path not valid UTF-8");
+                builder = builder.header(path);
+            }
+            Err(e) => eprintln!("Glob error: {:?}", e),
+        }
+    }
+
+    // Generate Rust bindings from the header
+    let bindings = builder.generate().expect("Unable to generate bindings");
+
+    // Write bindings to $OUT_DIR/bindings.rs
+    let out_path = std::path::PathBuf::from(std::env::var("OUT_DIR").unwrap());
+    bindings
+        .write_to_file(out_path.join("bindings.rs"))
+        .expect("Couldn't write bindings!");
+}
+
+fn main() {
+    // Compile C sources
+    compile_c_sources();
+
+    // Generate Rust bindings from the header
+    generate_bindings();
+}
